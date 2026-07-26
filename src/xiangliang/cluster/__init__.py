dccf17f45e9b7273365640ab1f2db8ea8e559b22
SQC Developer <dev@example.com>
2026-07-26 12:28:15 +0800

    chore: remove old xiangliang package after rename

--- src/xiangliang/cluster/__init__.py
+++
@@ -1,6 +0,0 @@
-"""xiangliang.cluster - 分布式向量数据库集群模块"""
-
-from xiangliang.cluster.consistent_hash import ConsistentHash
-from xiangliang.cluster.client import DistributedVectorDBClient
-
-__all__ = ["ConsistentHash", "DistributedVectorDBClient"]
--- src/xiangliang/storage/__init__.py
+++
@@ -1,4 +0,0 @@
-from xiangliang.storage.persistence import load_snapshot, save_snapshot
-from xiangliang.storage.wal import WALReader, WALWriter, truncate_wal
-
-__all__ = ["save_snapshot", "load_snapshot", "WALWriter", "WALReader", "truncate_wal"]
